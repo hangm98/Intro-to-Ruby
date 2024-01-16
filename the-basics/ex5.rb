850d8788aed818971c727e28a7564ec644f8cc6a
Hang Mai <mph23198@gmail.com>
2024-01-16 15:48:10 -0600

    Debug ex5.rb with pry and fixed, ex5.rb worked as expected.

--- the-basics/ex5.rb
+++ the-basics/ex5.rb
@@ -1,9 +1,14 @@
 #Factorial of 5, 6, 7, and 8
-for i in 5..8 do
-  num = i
-  while num >= 1
-    tmp = num * (num - 1)
+require 'pry'
+
+for i in 0..3 do
+  num = i+5
+  tmp = num
+  while num > 1
+    tmp = tmp * (num - 1)
     num = num - 1
+    # REPL session
+    #binding.pry
   end
-  puts "Factorial of #{i} is #{tmp}"
+  puts "Factorial of #{i+5} is #{tmp}"
 end
